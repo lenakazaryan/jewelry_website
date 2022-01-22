711058d1f617eee1af9e48ad4e17c16c979d29c4
Lina <linakazaryan2019@gmail.com>
2022-01-22 09:05:03 -0800

    change_headerPart

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "./App.css";
-import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import Main from "./components/Main/Main";
@@ -7,7 +6,6 @@ import Main from "./components/Main/Main";
 function App() {
   return (
     <div className="App">
-      <Header />
       <Navbar />
       <Main />
       <Footer />
